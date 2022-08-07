6f19b1c1658f84c99fe59b654eff0340aa3fb8c1
wendwin <wendwin12@gmail.com>
2022-08-07 17:26:21 +0700

    menahbahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quasi molestiae dignissimos illum quam magnam eaque eum, ipsa facere! Rerum.</p>
 </body>
 </html>
